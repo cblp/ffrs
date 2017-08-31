86e824099c877f63a96f14d769e426d8d3e168b9
Yuriy Syrovetskiy <cblp@cblp.su>
2017-08-31 17:25:41 +0300

    Add real tests on adding and retrieving

--- hs/exe/Main.hs
+++ hs/exe/Main.hs
@@ -1,6 +1,4 @@
 module Main where
 
-import           System.Environment (getEnvironment)
-
 main :: IO ()
-main = getEnvironment >>= print . filter (\(x, _) -> x == "HOME")
+main = pure ()
--- hs/test/Main.hs
+++ hs/test/Main.hs
@@ -4,17 +4,34 @@ import qualified Data.Map as Map
 import           System.Environment (getEnvironment)
 import           System.IO.Temp (withSystemTempDirectory)
 import           System.Process (env, proc, readCreateProcess)
-import           Test.Tasty (TestTree, defaultMain, testGroup)
-import           Test.Tasty.HUnit (testCase)
+import           Test.Tasty (TestName, TestTree, defaultMain, testGroup)
+import           Test.Tasty.HUnit (testCase, (@?=))
 
 main :: IO ()
-main = defaultMain $ testGroup "" [testAddAndRetrieve]
+main = defaultMain $ testGroup "" [addAndRetrieve, firstRun]
 
-testAddAndRetrieve :: TestTree
-testAddAndRetrieve =
-    testCase "add and retrieve" $ do
-        curEnv <- getEnvironment
-        withSystemTempDirectory "ff.test." $ \dir -> do
-            let env' = Map.assocs . Map.insert "HOME" dir $ Map.fromList curEnv
-            let ff args = (proc "ff" args){env = Just env'}
-            readCreateProcess (ff ["add"]) "" >>= putStrLn
+addAndRetrieve :: TestTree
+addAndRetrieve = testCaseRunFF "add and retrieve" $ \ff -> do
+    add1 <- ff ["add", "example"]
+    add1 @?= ""
+    list1 <- ff []
+    list1 @?= "1 example"
+    add2 <- ff ["add", "example"]
+    add2 @?= ""
+    list2 <- ff []
+    list2 @?= unlines ["1 example", "2 example"]
+
+firstRun :: TestTree
+firstRun = testCaseRunFF "first run" $ \ff -> do
+    list <- ff []
+    list @?= ""
+
+type Proc = [String] -> IO String
+
+testCaseRunFF :: TestName -> (Proc -> IO ()) -> TestTree
+testCaseRunFF name action = testCase name $ do
+    curEnv <- getEnvironment
+    withSystemTempDirectory "ff.test." $ \dir -> do
+        let env' = Map.assocs . Map.insert "HOME" dir $ Map.fromList curEnv
+        let ff args = readCreateProcess (proc "ff" args){env = Just env'} ""
+        action ff
